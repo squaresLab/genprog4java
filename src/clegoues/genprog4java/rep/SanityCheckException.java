a5438a142dea0d28e725afe1828e858805f3217e
Claire Le Goues <clegoues@cs.cmu.edu>
2014-01-31 11:21:03 -0500

    moar debug, and cleanup

--- src/clegoues/genprog4java/rep/SanityCheckException.java
+++
@@ -1,8 +0,0 @@
-package clegoues.genprog4java.rep;
-
-public class SanityCheckException extends Exception {
-// TODO: this probably needs something in here, no?
-	public SanityCheckException(String message) {
-		super(message);
-	}
-}
