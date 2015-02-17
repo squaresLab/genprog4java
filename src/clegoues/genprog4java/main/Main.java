fdba986d06bfd706ea4eb467d36d7145a3fa889e
Alex Maeda <amaeda@andrew.cmu.edu>
2015-02-17 13:58:17 -0500

    Am i pushing on the branch?

--- src/clegoues/genprog4java/main/Main.java
+++ src/clegoues/genprog4java/main/Main.java
@@ -75,6 +75,7 @@ public class Main {
 		try {
 			switch(Search.searchStrategy) {
 			case "ga": searchEngine.geneticAlgorithm(baseRep, incomingPopulation);
+			this won't compile now
 				break;
 			case "brute": searchEngine.bruteForceOne(baseRep);
 				break;
