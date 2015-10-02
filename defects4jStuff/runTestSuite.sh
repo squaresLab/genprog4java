868cb897e5e9ac46cf4ebc8b239d4153ff09621b
Mauricio Soto <mausotog@hotmail.com>
2015-10-02 14:56:51 -0400

    Trying to include Alex's changes to the code into the main branch, 1st attempt. It does run properly at this point

---
+++ defects4jStuff/runTestSuite.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#This file takes the bug code and runs it on the test suite in $DEFECTS4JDIR/ExamplesCheckedOut/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/pos.tests
+
+# 1st param is the project in upper case (ex: Lang, Chart, Closure, Math, Time)
+# 2nd param is the bug number (ex: 1,2,3,4,...)
+# 3rd param is the folder where the genprog project is (ex: "/home/mau/Research/genprog4java/" )
+# 4td param is the folder where defects4j is installed (ex: "/home/mau/Research/defects4j/" )
+
+#Mau runs it like this:
+#./runTestSuite.sh Math 2 /home/mau/Research/genprog4java/ /home/mau/Research/defects4j/
+
+PROJECT="$1"
+BUGNUMBER="$2"
+GENPROGDIR="$3"
+DEFECTS4JDIR="$4"
+
+LOWERCASEPACKAGE=`echo $PROJECT | tr '[:upper:]' '[:lower:]'`
+
+POSTESTS=$DEFECTS4JDIR/ExamplesCheckedOut/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/pos.tests
+
+JAVALOCATION=$(which java)
+
+
+while read p; do
+
+  echo Running test: $p
+OUTPUT=$($JAVALOCATION -cp .:$DEFECTS4JDIR/ExamplesCheckedOut/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/"$LOWERCASEPACKAGE"AllSourceClasses.jar:$DEFECTS4JDIR/ExamplesCheckedOut/"$LOWERCASEPACKAGE""$BUGNUMBER"Buggy/"$LOWERCASEPACKAGE"AllTestClasses.jar:$DEFECTS4JDIR/framework/projects/lib/junit-4.11.jar org.junit.runner.JUnitCore $p)
+
+echo $OUTPUT
+
+if [[ $OUTPUT == *"OK (0 tests)"* ]]
+then
+  echo "ERROR! IN THE CLASS: " $p
+  break
+fi
+
+if [[ $OUTPUT == *"Could not find class"* ]]
+then
+  echo "ERROR! IN THE CLASS: " $p
+  break
+fi
+
+if [[ $OUTPUT == *"OK ("* ]]
+then
+  echo "OK"
+else
+  echo "ERROR! IN THE CLASS: " $p
+  break
+fi
+
+
+done <$POSTESTS
+
+
+
+
+
