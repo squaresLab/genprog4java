e04fd843c5ec842488529c8b976c252cb9ad4c43
manishmotwani <manishmotwani@localhost>
2015-10-26 02:50:42 -0400

    added code for PAR template "Lower Bound Setter"

--- src/clegoues/genprog4java/mut/JavaEditOperation.java
+++ src/clegoues/genprog4java/mut/JavaEditOperation.java
@@ -264,19 +264,16 @@ public class JavaEditOperation implements
 
 			break;
 		case LBOUNDSET:
-
-			break;
-		case UBOUNDSET:
 			final Map<ASTNode, List<ASTNode>> nodestmts = new HashMap<ASTNode, List<ASTNode>>();	// to track the parent nodes of array access nodes
-			final Map<ASTNode, String> upperbound = new HashMap<ASTNode, String>();			// to set the upper-bound values of array. currently set to arrayname.length
-				
-			locationNode.accept(new ASTVisitor() {
+			final Map<ASTNode, String> lowerbound = new HashMap<ASTNode, String>();			// to set the upper-bound values of array. currently set to arrayname.length
+		
+			cu.accept(new ASTVisitor() {
 				
 				// method to visit all ArrayAccess nodes in locationNode and store their parents
 				public boolean visit(ArrayAccess node) {
-					upperbound.put(node, node.getArray().toString().concat(".length"));
-					ASTNode parent = getParent(node);				
-					if(!nodestmts.containsKey(parent)){		
+					lowerbound.put(node, "0");
+					ASTNode parent = getParent(node);
+					if(!nodestmts.containsKey(parent)){
 						List<ASTNode> arraynodes = new ArrayList<ASTNode>();
 						arraynodes.add(node);
 						nodestmts.put(parent, arraynodes);		
@@ -290,6 +287,7 @@ public class JavaEditOperation implements
 				}
 				
 				// method to get the parent of ArrayAccess node. We traverse the ast upwards until the parent node is an instance of statement
+				// if statement is(are) added to this parent node
 				private ASTNode getParent(ArrayAccess node) {
 					ASTNode parent = node.getParent();
 					while(!(parent instanceof Statement)){
@@ -300,11 +298,92 @@ public class JavaEditOperation implements
 			});
 			
 			Set<ASTNode> parentnodes = nodestmts.keySet();
-			// for each parent node which may have multiple array access instances 
+			// for each parent node which may have multiple array access instances
 			for(ASTNode parent: parentnodes){
 				// create a newnode
 				Block newnode = parent.getAST().newBlock();
 				List<ASTNode> arrays = nodestmts.get(parent);
+				
+				// for each of the array access instances
+				for( ASTNode  array : arrays){
+					
+					// get the array index
+					String arrayindex = ((ArrayAccess)array).getIndex().toString();
+					arrayindex = arrayindex.replace("++", "");
+					arrayindex = arrayindex.replace("--", "");
+					
+					// create if statement 
+					IfStatement stmt = parent.getAST().newIfStatement();
+					
+					// with expression "index < 0" 
+					InfixExpression expression = null;
+					expression = parent.getAST().newInfixExpression();
+					expression.setLeftOperand(parent.getAST().newSimpleName(arrayindex));
+					expression.setOperator(Operator.LESS);
+					expression.setRightOperand(parent.getAST().newNumberLiteral(lowerbound.get(array)));
+					stmt.setExpression(expression);
+					
+					// and then part as "index = 0"
+					Assignment thenexpression = null;
+					thenexpression = parent.getAST().newAssignment();
+					thenexpression.setLeftHandSide(parent.getAST().newSimpleName(arrayindex));
+					thenexpression.setOperator(Assignment.Operator.ASSIGN);
+					thenexpression.setRightHandSide(parent.getAST().newNumberLiteral(lowerbound.get(array)));
+					ExpressionStatement thenstmt = parent.getAST().newExpressionStatement(thenexpression);
+					stmt.setThenStatement(thenstmt);
+					
+					// add if statement to newnode
+					newnode.statements().add(stmt);
+				}
+				// append the existing content of parent node to newnode
+				ASTNode stmt = (Statement)parent;
+				stm1 = ASTNode.copySubtree(parent.getAST(), stmt);
+				newnode.statements().add(stmt);
+				System.out.println("new node  :" + newnode.toString()); 
+				rewriter.replace(parent, newnode, null);
+			}	
+				
+			break;
+		case UBOUNDSET:
+			final Map<ASTNode, List<ASTNode>> nodestmts1 = new HashMap<ASTNode, List<ASTNode>>();	// to track the parent nodes of array access nodes
+			final Map<ASTNode, String> upperbound = new HashMap<ASTNode, String>();			// to set the upper-bound values of array. currently set to arrayname.length
+				
+			locationNode.accept(new ASTVisitor() {
+				
+				// method to visit all ArrayAccess nodes in locationNode and store their parents
+				public boolean visit(ArrayAccess node) {
+					upperbound.put(node, node.getArray().toString().concat(".length"));
+					ASTNode parent = getParent(node);				
+					if(!nodestmts1.containsKey(parent)){		
+						List<ASTNode> arraynodes = new ArrayList<ASTNode>();
+						arraynodes.add(node);
+						nodestmts1.put(parent, arraynodes);		
+					}else{
+						List<ASTNode> arraynodes = (List<ASTNode>) nodestmts1.get(parent);
+						if(!arraynodes.contains(node))
+							arraynodes.add(node);
+						nodestmts1.put(parent, arraynodes);	
+					}
+					return true;
+				}
+				
+				// method to get the parent of ArrayAccess node. We traverse the ast upwards until the parent node is an instance of statement
+				// if statement is(are) added to this parent node
+				private ASTNode getParent(ArrayAccess node) {
+					ASTNode parent = node.getParent();
+					while(!(parent instanceof Statement)){
+						parent = parent.getParent();
+					}
+					return parent;
+				}
+			});
+			
+			Set<ASTNode> parentnodes1 = nodestmts1.keySet();
+			// for each parent node which may have multiple array access instances 
+			for(ASTNode parent: parentnodes1){
+				// create a newnode
+				Block newnode = parent.getAST().newBlock();
+				List<ASTNode> arrays = nodestmts1.get(parent);
 				System.out.println("original parent: " + parent.toString());
 				
 				// for each of the array access instances
@@ -315,7 +394,7 @@ public class JavaEditOperation implements
 					arrayindex = arrayindex.replace("++", "");
 					arrayindex = arrayindex.replace("--", "");
 					
-					// create the if statement 
+					// create if statement 
 					IfStatement stmt = parent.getAST().newIfStatement();
 					
 					// with expression "index > arrayname.length" 
@@ -351,7 +430,7 @@ public class JavaEditOperation implements
 					// add if statement to newnode
 					newnode.statements().add(stmt);
 				}
-				// append the existing content of parent to newnode
+				// append the existing content of parent node to newnode
 				ASTNode stmt = (Statement)parent;
 				stmt = ASTNode.copySubtree(parent.getAST(), stmt);
 				newnode.statements().add(stmt);
