ad9d9df1294e9955597a06c31749bcaa80e5dd92
Mauricio Soto <msotogon@andrew.cmu.edu>
2015-11-23 13:26:03 -0500

    bash file to run SimpleExample easier

---
+++ SimpleExample/prepareSimpleExample.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# 1st param is the path to the SimpleExample folder
+# 2nd param is the path to genprog for java
+
+#Example of how to run it:
+#./prepareSimpleExample.sh /home/mau/Research/genprog4java
+
+
+PATHTOGENPROG="$1"
+PATHTOSIMPLEEXAMPLE=$PATHTOGENPROG"/SimpleExample"
+
+#Crete jar file
+cd $PATHTOSIMPLEEXAMPLE/src/
+jar cf $PATHTOSIMPLEEXAMPLE/SimpleExample.jar ./* 
+
+
+#Create config file
+cd $PATHTOSIMPLEEXAMPLE
+
+PACKAGEDIR=${JAVADIR//"/"/"."}
+
+#Create config file 
+FILE="$PATHTOSIMPLEEXAMPLE"/simpleExample.config
+/bin/cat <<EOM >$FILE
+popsize = 20
+seed = 0
+testsDir = tests/
+javaVM = /usr/bin/java
+workingDir = $PATHTOSIMPLEEXAMPLE/
+outputDir = $PATHTOSIMPLEEXAMPLE/tmp/
+libs = $PATHTOSIMPLEEXAMPLE/SimpleExample.jar:$PATHTOGENPROG/lib/junit-4.10.jar:$PATHTOGENPROG/lib/junittestrunner.jar
+sanity = yes
+regenPaths
+sourceDir = src/
+positiveTests = $PATHTOSIMPLEEXAMPLE/pos.tests
+negativeTests = $PATHTOSIMPLEEXAMPLE/neg.tests
+jacocoPath = $PATHTOGENPROG/lib/jacocoagent.jar
+classSourceFolder = target/classes
+targetClassName = packageSimpleExample.SimpleExample
+EOM
+
+
+
+
+
