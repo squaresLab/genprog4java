5ce3a6512a655aa864e3b96358276b4f5d54ce45
Mauricio Soto <mausotog@hotmail.com>
2015-11-23 11:13:13 -0500

    forgot to add the SimpleExample to the repository

---
+++ SimpleExample/src/packageSimpleExample/SimpleExampleTestsNeg.java
@@ -0,0 +1,20 @@
+package packageSimpleExample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimpleExampleTestsNeg {
+
+  @Test
+  public void severalTestCasesToTestMid() {
+
+    // SimpleExample is tested
+    SimpleExample tester = new SimpleExample();
+
+    // assert statements
+    assertEquals("Should be 2", 2, tester.mid(2,1,3)); 
+ 
+   }
+
+} 
