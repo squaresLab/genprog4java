85fcd4c0e1a90208d1c8a2ec04bcdc58fa7bfdd0
Mauricio Soto <msotogon@andrew.cmu.edu>
2015-12-09 17:16:02 -0500

    Returned to the original simpler SimpleExample

--- SimpleExample/src/packageSimpleExample/SimpleExample.java
+++ SimpleExample/src/packageSimpleExample/SimpleExample.java
@@ -4,15 +4,12 @@ public class SimpleExample {
 
     public int mid(int x, int y, int z){
 
-	final String noUse = "NoUse";
         int ret;
 	ret = z;
 	if(y<z){
 	   if(x<y){
 		ret = y;
 	   }else if(x<z){
-		String s = null;
-		String len = s.replaceAll("", noUse).toString();
 		ret = y; // bug, it should be ret = x;
 	   }
 	}else{
@@ -24,10 +21,4 @@ public class SimpleExample {
 	}
 	return ret;
     }
-/*
-    public static void main(String[] args) {
-        SimpleExample s = new SimpleExample();
-	println("The middle value is: " + s.mid(2,3,1));
-    }
-*/
 }
