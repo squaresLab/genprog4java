aa886177ee62c6d669ebadbaaf4d45c063712b45
Claire Le Goues <clegoues@cs.cmu.edu>
2016-04-06 14:29:11 -0400

    whoops, this one was all me.  I probably just fixed defects4j for Manish, let me double check...

--- defects4j-scripts/prepareBug.sh
+++ defects4j-scripts/prepareBug.sh
@@ -88,8 +88,6 @@ cd $BUGWD/$WD
 FILE="$DEFECTS4JDIR"/ExamplesCheckedOut/$LOWERCASEPACKAGE$2Buggy/runCompile.sh
 /bin/cat <<EOM >$FILE
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/
-export PATH=$JAVA_HOME/bin/:$PATH
 cd $DEFECTS4JDIR/ExamplesCheckedOut/$LOWERCASEPACKAGE$2Buggy/
 $DEFECTS4JDIR/framework/bin/defects4j compile
 EOM
