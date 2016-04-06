0e9595bf4b82c2b751b96f03b27c66bc4eb91a7d
Claire Le Goues <clegoues@cs.cmu.edu>
2016-04-06 18:06:37 -0400

    removing my ability to make that same mistake again

--- src/clegoues/genprog4java/fitness/TestCase.java
+++ src/clegoues/genprog4java/fitness/TestCase.java
@@ -36,24 +36,15 @@ package clegoues.genprog4java.fitness;
 
 public class TestCase {
 	private TestType posOrNeg;
-	private int testNum = -1;
 	private String testName = null;
-	public TestCase(TestType t, int num) {
-		this.posOrNeg = t;
-		this.testNum = num;
-	}
+
 	public TestCase(TestType t, String name) {
 		this.posOrNeg = t;
 		this.testName = name; 
 	}
 	public String toString () {
-		if(this.testName != null)
-			return this.testName;
-		if(posOrNeg == TestType.POSITIVE) {
-			return "p" + this.testNum;
-		} else {
-			return "n" + this.testNum;
-		}
+		return this.testName;
+
 	}
-	
+
 }
