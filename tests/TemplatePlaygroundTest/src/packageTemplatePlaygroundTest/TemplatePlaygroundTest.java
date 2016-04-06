3a958104bbaa147cb2606b523f5580cb474ecbf7
Claire Le Goues <clegoues@cs.cmu.edu>
2016-04-05 20:54:32 -0400

    TestClassPath instead of testDir/separate classpath construction, propagated to templatePlayground config

--- tests/TemplatePlaygroundTest/src/packageTemplatePlaygroundTest/TemplatePlaygroundTest.java
+++ tests/TemplatePlaygroundTest/src/packageTemplatePlaygroundTest/TemplatePlaygroundTest.java
@@ -1,23 +1,33 @@
 package packageTemplatePlaygroundTest;
 
 import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
 
 public class TemplatePlaygroundTest {
 
+	private int myfield;
+	
     public TemplatePlaygroundTest(){
-    	this(1);
     }
     
+    public int addOne(int i) {
+    	return i + 1;
+    }
+    
+    public int addTwo(int j) {
+    	return j + 2;
+    }
     public TemplatePlaygroundTest(int i){
-    	if(1==1){
+    	if(i < 0){
     		throw new IllegalArgumentException("Final speed can not be less than zero");
     	}
     }
 
     public int mid(int x, int y, int z){
+    	int foo = 0;
+    	foo = addOne(foo);
 
-        int ret;
-	ret = z;
+    	int ret = z;
 	if(y<z){
 	   if(x<y){
 		ret = y;
