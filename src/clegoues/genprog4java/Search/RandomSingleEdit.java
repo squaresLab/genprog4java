0fe11b0ef024ec9fcd76e385347023dcde28ec85
clegoues <clegoues@localhost>
2016-06-12 16:10:14 -0400

    actually implement random mutation in the associated search strategy

--- src/clegoues/genprog4java/Search/RandomSingleEdit.java
+++ src/clegoues/genprog4java/Search/RandomSingleEdit.java
@@ -1,28 +1,47 @@
 package clegoues.genprog4java.Search;
 
+import static clegoues.util.ConfigurationBuilder.INT;
+
 import clegoues.genprog4java.fitness.Fitness;
 import clegoues.genprog4java.mut.EditOperation;
 import clegoues.genprog4java.rep.Representation;
+import clegoues.util.ConfigurationBuilder;
 
 public class RandomSingleEdit<G extends EditOperation> extends Search<G>{
 
+	
+	protected static int maxVariants = ConfigurationBuilder.of( INT )
+			.withVarName( "maxVariants" )
+			.withDefault( "400" )
+			.withHelp( "maximum number of variants to consider" )
+			.inGroup( "Search Parameters" )
+			.build();
+	
 	public RandomSingleEdit(Fitness<G> engine) {
 		super(engine);
-		
+		engine.initializeModel();
 	}
 
 	@Override
 	protected Population<G> initialize(Representation<G> original, Population<G> incomingPopulation)
 			throws RepairFoundException {
-		// FIXME: register just the basic mutations?
+		original.reduceSearchSpace();
 		return null;
 	}
 
 	@Override
 	protected void runAlgorithm(Representation<G> original, Population<G> initialPopulation)
 			throws RepairFoundException {
-		// TODO Auto-generated method stub
-		
+		int numVariantsConsidered = 0;
+		while(numVariantsConsidered < RandomSingleEdit.maxVariants) {
+			Representation<G> variant = original.copy();
+			mutate(variant);
+			if (fitnessEngine.testFitness(0, variant)) {
+				this.noteSuccess(variant, original, 0);
+					if(!continueSearch) 
+						return;
+				}
+			}
 	}
 
 
