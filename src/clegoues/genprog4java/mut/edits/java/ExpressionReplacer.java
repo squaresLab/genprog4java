59c9209e904ee18f1aaf3145a1a6975700450ea8
clegoues <clegoues@localhost>
2016-06-16 19:36:19 -0400

    conditional expression replacement is easier than I thought it was.

--- src/clegoues/genprog4java/mut/edits/java/ExpressionReplacer.java
+++ src/clegoues/genprog4java/mut/edits/java/ExpressionReplacer.java
@@ -2,24 +2,15 @@ package clegoues.genprog4java.mut.edits.java;
 
 import java.util.HashMap;
 
-import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
 import clegoues.genprog4java.mut.EditHole;
 import clegoues.genprog4java.mut.Mutation;
 import clegoues.genprog4java.mut.holes.java.JavaLocation;
 
-public class ExpressionReplacer extends JavaEditOperation {
+public class ExpressionReplacer extends MethodParameterReplacer {
 
 	
 	public ExpressionReplacer(JavaLocation location,  HashMap<String, EditHole> sources) {
 		super(Mutation.EXPREP, location, sources);
-		this.holeNames.add("expReplace");
 	}
-
-	@Override
-	public void edit(ASTRewrite rewriter) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
