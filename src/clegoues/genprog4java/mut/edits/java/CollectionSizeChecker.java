9e1ddf08f7815b9055e7bf9fa2adc46aeb90d7ef
Claire Le Goues <clegoues@cs.cmu.edu>
2016-06-30 12:35:11 -0400

    adding class defs for last two templates

---
+++ src/clegoues/genprog4java/mut/edits/java/CollectionSizeChecker.java
@@ -0,0 +1,13 @@
+package clegoues.genprog4java.mut.edits.java;
+
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+
+public class CollectionSizeChecker extends JavaEditOperation {
+
+	@Override
+	public void edit(ASTRewrite rewriter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
