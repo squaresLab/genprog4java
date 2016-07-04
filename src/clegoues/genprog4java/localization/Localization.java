06929420185702dfe3f6ead2bccf0ad82b78a8e5
clegoues <clegoues@localhost>
2016-07-04 11:18:32 -0400

    epic localization refactor to support lm integration.  Doesn't work yet.

---
+++ src/clegoues/genprog4java/localization/Localization.java
@@ -0,0 +1,37 @@
+package clegoues.genprog4java.localization;
+
+import static clegoues.util.ConfigurationBuilder.STRING;
+
+import java.util.ArrayList;
+
+import clegoues.genprog4java.Search.GiveUpException;
+import clegoues.genprog4java.mut.Location;
+import clegoues.genprog4java.rep.WeightedAtom;
+import clegoues.util.ConfigurationBuilder;
+
+public abstract class Localization {
+
+	
+	public static final ConfigurationBuilder.RegistryToken token =
+			ConfigurationBuilder.getToken();
+
+	public static String fixStrategy = ConfigurationBuilder.of ( STRING )
+			.withVarName("fixStrategy")
+			.withHelp("Fix source strategy")
+			.withDefault("classScope")
+			.inGroup( "Localization Parameters" )
+			.build();
+
+	public static String faultStrategy = ConfigurationBuilder.of ( STRING )
+			.withVarName("faultStrategy")
+			.withHelp("Fault localization strategy")
+			.withDefault("classScope")
+			.inGroup( "Localization Parameters" )
+			.build();
+	
+	public abstract ArrayList<Location> getFaultLocalization();
+	public abstract void reduceSearchSpace() throws GiveUpException;
+	public abstract ArrayList<WeightedAtom> getFixSourceAtoms();
+	public abstract void setAllPossibleStmtsToFixLocalization();
+
+}
