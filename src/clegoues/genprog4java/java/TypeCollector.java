249940106a5dcd23db20dc9b022d4c60238658a4
Claire Le Goues <clegoues@cs.cmu.edu>
2016-07-04 23:40:28 -0400

    other package infos.  Still filling them in; this commit just provides the files

--- src/clegoues/genprog4java/java/TypeCollector.java
+++ src/clegoues/genprog4java/java/TypeCollector.java
@@ -47,28 +47,26 @@ import org.eclipse.jdt.core.dom.SimpleName;
 public class TypeCollector extends ASTVisitor
 {
 	private Set<String> typeSet;
-	
+
 	public TypeCollector(Set<String> o)
 	{
 		this.typeSet = o;
 	}
-	
+
 	@Override
 	public boolean visit(SimpleName node)
 	{
 		IBinding nodeBinding = node.resolveBinding();
 		ITypeBinding typeBinding = node.resolveTypeBinding();
-		
+
 		if(nodeBinding != null && typeBinding != null && 
 				nodeBinding instanceof IVariableBinding && 
 				!typeBinding.isPrimitive())
 		{
 			String type = typeBinding.getQualifiedName();
-			
-			if(!type.equals("java.lang.String"))
-				typeSet.add(type);
+			typeSet.add(type);
 		}
-		
+
 		return super.visit(node);
 	}
 }
