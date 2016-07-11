03f8cb544687d7fada94c0fe67c1f48043d604f4
clegoues <clegoues@localhost>
2016-07-11 14:55:03 -0400

    size check mostly working

--- tests/TemplatePlaygroundTest/src/packageTemplatePlaygroundTest/TemplatePlaygroundTest.java
+++ tests/TemplatePlaygroundTest/src/packageTemplatePlaygroundTest/TemplatePlaygroundTest.java
@@ -5,15 +5,13 @@ public class TemplatePlaygroundTest {
 
 	public int mid(int x, int y, int z){
 		int ret = z;
-
+		ArrayList<Integer> newArrayList = new ArrayList<Integer>(5);
 		if(y<z){
 			if(x<y){
 				ret = y;
 			}else if(x<z  && y > z){
-				//ret = y; // bug, it should be ret = x;
-				for(int i = 0; i < 5; i++) {
+				newArrayList.add(0,5);
 					ret = 5;
-				}
 			}
 		}else{
 			if(x>y){
