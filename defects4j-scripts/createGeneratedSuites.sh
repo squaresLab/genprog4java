8ffc0b34bcb17265027c498af85137181e8b7137
mausoto <msotogon@andrew.cmu.edu>
2016-07-17 00:44:02 -0400

    merging with the version in the server

--- defects4j-scripts/createGeneratedSuites.sh
+++ defects4j-scripts/createGeneratedSuites.sh
@@ -6,51 +6,57 @@
 GENPROGDIR="$1"
 DEFECTS4JDIR="$2"
 
-rm -rf "$DEFECTS4JDIR"/testGenerated/
-rm -rf /tmp/*
+#rm -rf "$DEFECTS4JDIR"/testGenerated/
+#rm -rf /tmp/*
 
-rm -rf $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites
-mkdir $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites
+rm -rf $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites.txt
+mkdir $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites.txt
+STARTAT=1
 
-for (( projectNumb=1 ; projectNumb<=5 ; projectNumb++ ))
+for (( projectNumb=2 ; projectNumb<=2 ; projectNumb++ ))
 do
   case "$projectNumb" in
   "1" )
     PROJECT="Math"
     NUMBEROFBUGS=106
+    STARTAT=61
   ;;
   "2" ) 
     PROJECT="Lang"
-    NUMBEROFBUGS=65
+    NUMBEROFBUGS=42
+    STARTAT=42
   ;;
   "3" ) 
     PROJECT="Chart"
     NUMBEROFBUGS=26
+    STARTAT=1
   ;;
   "4" ) 
     PROJECT="Time"
     NUMBEROFBUGS=27
+    STARTAT=12
   ;;
   "5" ) 
     PROJECT="Closure"
-    NUMBEROFBUGS=133
+    NUMBEROFBUGS=80
+    STARTAT=80
   ;;
   esac
 
-  for (( bug=1 ; bug<=$NUMBEROFBUGS ; bug++ ))
+  for (( bug=$STARTAT ; bug<=$NUMBEROFBUGS ; bug++ ))
   do
   
     $GENPROGDIR/defects4j-scripts/prepareBug.sh $PROJECT $bug $GENPROGDIR $DEFECTS4JDIR generated 100 $DEFECTS4JDIR/ExamplesCheckedOut/
 
-    echo ""
-    echo "prepareBug.sh finished executing"
+#    echo ""
+#    echo "prepareBug.sh finished executing"
 
-    $GENPROGDIR/defects4j-scripts/runTestSuite.sh $PROJECT $bug  $GENPROGDIR $DEFECTS4JDIR > $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites/resultsFromRunningGenereatedTestSuites"$PROJECT""$bug".txt
+#    $GENPROGDIR/defects4j-scripts/runTestSuite.sh $PROJECT $bug  $GENPROGDIR $DEFECTS4JDIR > $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites/resultsFromRunningGenereatedTestSuites"$PROJECT""$bug".txt
 
-    echo ""
-    echo "Generated test suite has been ran on the buggy code."
-    echo "Results of $PROJECT $bug stored in: $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites/resultsFromRunningGenereatedTestSuites"$PROJECT""$bug".txt"
-    echo ""
+#    echo ""
+#    echo "Generated test suite has been ran on the buggy code."
+#    echo "Results of $PROJECT $bug stored in: $DEFECTS4JDIR/ResultsFromRunningGenereatedTestSuites/resultsFromRunningGenereatedTestSuites"$PROJECT""$bug".txt"
+#    echo ""
 
   done
 done
