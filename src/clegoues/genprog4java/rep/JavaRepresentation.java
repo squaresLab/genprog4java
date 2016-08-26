6fbbad514fd368ef60bdb2a886ec8055a2e8863c
mausoto <msotogon@andrew.cmu.edu>
2016-08-26 01:17:49 -0400

    fixing getting stuck in waitfor() in a java 7 compliant way, still cant reproduce the error

--- src/clegoues/genprog4java/rep/JavaRepresentation.java
+++ src/clegoues/genprog4java/rep/JavaRepresentation.java
@@ -583,7 +583,7 @@ FaultLocRepresentation<JavaEditOperation> {
 				return true;
 			}
 		} else {
-			return GlobalUtils.runCommand(Configuration.compileCommand, timeCompilingOriginal*10);
+			return GlobalUtils.runCommand(Configuration.compileCommand);
 		}
 	}
 
