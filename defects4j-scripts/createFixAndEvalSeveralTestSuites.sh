835210dbf9c1f4c857a162fbbf6d8d8648b921c2
Mauricio Soto <mausotog@hotmail.com>
2016-10-06 18:39:58 -0400

    some small differences on a script while merging

--- defects4j-scripts/createFixAndEvalSeveralTestSuites.sh
+++ defects4j-scripts/createFixAndEvalSeveralTestSuites.sh
@@ -36,15 +36,16 @@ touch $DEFECTS4JDIR/generatedTestSuites/$LOWERCASERANDOOPOREVOSUITE/$IDENTIFIER/
 #All bugs with fix found:
 #declare -a arr=("Chart 21" "Lang 59" "Math 24" "Math 29" "Math 49" "Time 19" "Closure 125" "Closure 86" "Math 7" "Closure 83" "Lang 45" "Chart 3" "Chart 5" "Lang 39" "Math 40" "Closure 66" "Lang 43" "Chart 1" "Math 18" "Lang 10" "Math 20" "Lang 7" "Math 73" "Math 95" "Math 82" "Closure 46" "Lang 22" "Chart 26" "Chart 25" "Lang 63" "Chart 13" "Closure 107" "Closure 115" "Closure 126" "Closure 13" "Closure 19" "Closure 21" "Closure 22" "Math 28" "Math 50" "Math 80" "Math 81" "Math 85" "Math 8")
 #bugs we havent found a fix for yet
-#declare -a arr=("Closure 13" "Closure 21" "Closure 83" "Closure 107" "Closure 115" "Closure 125" "Closure 126" "Math 18" "Math 28" "Math 29" "Math 80" "Math 81" "Math 20")
+#declare -a arr=("Closure 13" "Closure 21" "Closure 107" "Closure 115" "Closure 125" "Closure 126" "Math 18" "Math 28" "Math 80" "Math 81" "Math 20")
 #specific ones
-declare -a arr=("Time 19")
+declare -a arr=("Math 28")
 ## now loop through the above array
 for i in "${arr[@]}"
 do
   echo "Si:"
   echo ""
-  COM="./cfeIndividual.sh "$i" $1 $2 $3 $4 $5 $6 $7 $8 "
+
+  COM="./cfeIndividual.sh "$i" $1 $2 $3 $4 $5 $6 $7 $8 " 
 #&>> $DEFECTS4JDIR/generatedTestSuites/$LOWERCASERANDOOPOREVOSUITE/$IDENTIFIER/resultsEvaluatingSeveralTestSuites.txt"
   echo "$COM"
   eval $COM
