29e5ad880eceeb6d34b13601248489e6ad8590ff
mausoto <msotogon@andrew.cmu.edu>
2017-01-23 17:57:59 -0500

    Fixed the script that pulls the variant numbers

--- defects4j-scripts/probVsEDSpecificScripts/convertToCSVtheLastLines.sh
+++ defects4j-scripts/probVsEDSpecificScripts/convertToCSVtheLastLines.sh
@@ -5,25 +5,44 @@ if [ "$#" -ne 1 ]; then
     exit 0
 fi
 
-cd "$1"
-lastLinesFile=lastLinesOfAllLogs.txt
-lookingForRepair=0
-while read lineInLastLinesFile
-do
 
-if [[ $lineInLastLinesFile == "Seed"* ]] && [[ $lookingForRepair == 0 ]]
-then
-  lookingForRepair=1
-else if [[ $lineInLastLinesFile == "Seed"* ]] &&  [[ $lookingForRepair == 1 ]]
-then
-printf '%s\n' "Not found" | paste -sd " " >> Results.csv
-fi
-
-if [[ $lineInLastLinesFile == "Repair Found:"* ]]
-then 
-lookingForRepair = 0
-tmp=${lineInLastLinesFile#*variant}
-variantNumber=${tmp%)*}
-printf '%s\n' $variantNumber | paste -sd " " >> Results.csv
-fi
-done < $lastLinesFile
+dirToWork="$1"
+cd "$dirToWork"
+  rm -f Results.csv
+  lastLinesFile="lastLinesOfAllLogs.txt"
+  lookingForRepair=0
+  variantNum="Check Manually"
+  while read lineInLastLinesFile
+  do
+    if [[ $lineInLastLinesFile == *"variant"* ]]
+	then
+      tmp=${lineInLastLinesFile#*variant}
+      variantNumber=${tmp%)*}
+	  
+      #variantNum=$(echo $lineInLastLinesFile | cut -d variant -f 2 | cut -d ')' -f 1)
+	fi
+    if [[ $lineInLastLinesFile == "Seed"* ]]
+    then
+	  variantNum="Check Manually"
+	  if [[ $lookingForRepair == 0 ]]
+      then	  
+        lookingForRepair=1
+      else 
+	    echo "Not found" >> Results.csv
+        #printf '%s\n' "Not found" | paste -sd " " >> Results.csv
+      fi
+	fi
+    if [[ $lineInLastLinesFile == "Repair Found:"* ]]
+    then 
+      lookingForRepair=0
+      echo " $variantNumber" >> Results.csv
+	  #printf '%s\n' $variantNumber | paste -sd " " >> Results.csv
+    fi
+  done < $lastLinesFile
+  if [[ $lookingForRepair == 0 ]]
+  then	  
+    echo " $variantNumber" >> Results.csv
+  else 
+    echo "Not found" >> Results.csv
+  fi
+cd ..
