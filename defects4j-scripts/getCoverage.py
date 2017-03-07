756459935c78d7d56b34073e132c41261e997929
Mauricio Soto <mausotog@hotmail.com>
2017-03-07 17:31:36 -0500

    getCoverage.py running

--- defects4j-scripts/getCoverage.py
+++ defects4j-scripts/getCoverage.py
@@ -5,28 +5,30 @@ import os
 import xml.etree.ElementTree
 import subprocess
 
-#Get the modified classes from defects -p export SOMEATTRIBUTES classes.modified. They are in stdout. 
-
-def getOptions():
-	parser = argparse.ArgumentParser(description="")
-	parser.add_argument("project", help="1st param is the project in upper case (ex: Lang, Chart, Closure, Math, Time)")
-	parser.add_argument("bugNum", help="2nd param is the bug number (ex: 1,2,3,4,...)")
-	parser.add_argument("--genTool", help="3th param is the generation tool (Randoop or Evosuite)", default="Randoop")
-	parser.add_argument("--buggyFolder", help="4th param is the path of the buggy folder, starting from the the D4J_HOME folder (Example: ExamplesCheckedOut or BugsWithAFix)")
-	parser.add_argument("--testSuiteFolder", help="5th param is the path where the test suite is located, starting from the the D4J_HOME folder (Example: generatedTestSuites)")
-	parser.add_argument("--coverage", help="a coverage file")
-        parser.add_argument("--file1", help="test parameter, first file to diff")
-        parser.add_argument("--file2", help="test parameter, second file to diff")
-	return parser.parse_args()
-
 class BugInfo(object):
-	def __init__(self, project, d4jDir, pathToFix):
+	def __init__(self, project, bugNum, d4jDir, buggyFolder, fixedFolder, defects4jCommand):
 		self.project = project
+		self.bugNum = bugNum
 		self.d4jDir = d4jDir
-		self.pathToFix = pathToFix
+		self.buggyFolder = buggyFolder
+		self.fixedFolder = fixedFolder
+		self.defects4jCommand = defects4jCommand
+		self.ensureVersionAreCheckedOut()
 
 	def fixedPath(self):
 		return str(os.path.join(self.d4jDir, self.pathToFix))
+	
+	def ensureVersionAreCheckedOut(self):
+		if(not os.path.exists(self.buggyFolder)):
+			self.checkout(self.buggyFolder, "b")
+		if(not os.path.exists(self.fixedFolder)):
+			self.checkout(self.fixedFolder, "f")
+
+	def checkout(self, folderToCheckout, vers):
+		cmd = self.defects4jCommand + " checkout -p " + self.project + " -v " + self.bugNum + vers + " -w " + self.d4jDir + "/" + folderToCheckout
+		p = subprocess.Popen(cmd, shell=True) #, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		
+
 
 def computeCoverage(args):
         numberOfLinesChanged=0
@@ -46,13 +48,35 @@ def generateCovXML(d4j, bug, tool):
 	cmd = d4j + " coverage -w " + bug.fixedPath() + " -s " +  bug.suitePath(tool) # note that suitepath doesn't exist yet
 	subprocess.call(cmd, shell=True) # this doesn't save the log or do any kind of error checking (yet!)
 	
+def getEditedFiles(d4j):
+	cmd = d4j + " export -p classes.modified"
+	p = subprocess.Popen(cmd, shell=True, cwd="/home/mau/Research/defects4j/ExamplesCheckedOut/closure35FixedPatched", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	realpaths = [ line.strip().replace(".", "/") + ".java" for line in p.stdout ]
+	return realpaths
+
 # assume that file1, file2 are java files
 def getADiff(file1, file2):
         cmd = "diff  --unchanged-line-format=\"\"  --old-line-format=\"%dn \" --new-line-format=\"%dn \" " + file1 +" " + file2
         print cmd
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         for line in p.stdout:
-                print line
+		print line
+
+def getOptions():
+	parser = argparse.ArgumentParser(description="")
+	parser.add_argument("project", help="1st param is the project in upper case (ex: Lang, Chart, Closure, Math, Time)")
+	parser.add_argument("bugNum", help="2nd param is the bug number (ex: 1,2,3,4,...)")
+	parser.add_argument("buggyFolder", help="folder to check out buggy version of the bug")
+        parser.add_argument("fixedFolder", help="folder to check out fixed version of the bug")
+	parser.add_argument("--genTool", help="3th param is the generation tool (Randoop or Evosuite)", default="Randoop")
+	parser.add_argument("--testSuiteFolder", help="5th param is the path where the test suite is located, starting from the the D4J_HOME folder (Example: generatedTestSuites)")
+	parser.add_argument("--coverage", help="a coverage file")
+        parser.add_argument("--file1", help="test parameter, first file to diff")
+        parser.add_argument("--file2", help="test parameter, second file to diff")
+       
+	return parser.parse_args()
+
+
 def main():
 	args=getOptions()
 	# insert error handling/sanity checking to be sure the appropriate environment variables are set and abort with an error/usage message if not
@@ -60,6 +84,13 @@ def main():
 	# and make your argument description sentences shorter
 
 	d4jHome = os.environ['D4J_HOME']
+	defects4jCommand = d4jHome + "/framework/bin/defects4j"
+	bug = BugInfo(args.project, args.bugNum, d4jHome, args.buggyFolder, args.fixedFolder, defects4jCommand)
+	
+
+#	def __init__(self, project, bugNum, d4jDir, buggyFolder, fixedFolder):
+	editedFiles = getEditedFiles(defects4jCommand)
+
         if(not(args.file1 is None) and (not (args.file2 is None))):
                 getADiff(args.file1, args.file2)
 	print "the project you specified is: " + args.project
