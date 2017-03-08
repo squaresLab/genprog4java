7ea65eb8d8e8958b813dea8d6c0f36520137e527
Mauricio Soto <mausotog@hotmail.com>
2017-03-07 23:20:53 -0500

    now it generates the coverage XML file

--- defects4j-scripts/getCoverage.py
+++ defects4j-scripts/getCoverage.py
@@ -9,18 +9,28 @@ d4jHome = os.environ['D4J_HOME']
 defects4jCommand = d4jHome + "/framework/bin/defects4j"
 
 class BugInfo(object):
-	def __init__(self, project, bugNum, buggyFolder, fixedFolder):
+	def __init__(self, project, bugNum, buggyFolder, fixedFolder, testSuitePath):
 		self.project = project
 		self.bugNum = bugNum
 		self.buggyFolder = buggyFolder
 		self.fixedFolder = fixedFolder
+		self.testSuitePath = testSuitePath
 		self.ensureVersionAreCheckedOut()
+
+	def getProject(self):
+		return str(self.project)
+
+	def getBugNum(self):
+		return str(self.bugNum)
 
 	def getFixPath(self):
 		return str(os.path.join(d4jHome, self.fixedFolder))
 
 	def getBugPath(self):
 		return str(os.path.join(d4jHome, self.buggyFolder))
+
+	def getTestSuitePath(self):
+		return str(os.path.join(d4jHome, self.testSuitePath))
 	
 	def ensureVersionAreCheckedOut(self):
 		if(not os.path.exists(self.buggyFolder)):
@@ -33,21 +43,23 @@ class BugInfo(object):
 		p = subprocess.call(cmd, shell=True) #, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		
 def computeCoverage(args):
-        numberOfLinesChanged=0
-	numberOfLinesCovered=0
-
 	if(not (args.coverage is None)):
 		e = xml.etree.ElementTree.parse(args.coverage).getroot()
 		print e
 		lines = e.findall(".//line")
-		linesWereLookingFor = [103, 105, 106]
+		#linesWereLookingFor = [103, 105, 106]
 		realLines = [line for line in lines if int(line.attrib['number']) in linesWereLookingFor]
 		for realLine in realLines:
 			# check if covered
 			print realLine
 
 def generateCovXML(bug, tool):
-	cmd = defects4jCommand + " coverage -w " + bug.getFixPath() + " -s " +  bug.suitePath(tool) # note that suitepath doesn't exist yet
+	if(tool == "Evosuite"):
+		testSuiteName="evosuite-branch"
+	elif(tool == "Randoop"):
+		testSuiteName="randoop"
+	seed=1
+	cmd = defects4jCommand + " coverage -w " + bug.getFixPath() + " -s " +  bug.getTestSuitePath()+bug.getProject()+"-"+bug.getBugNum()+"f-"+testSuiteName+"."+str(seed)+".tar.bz2"
 	subprocess.call(cmd, shell=True) # this doesn't save the log or do any kind of error checking (yet!)
 
 def getEditedFiles(bug):
@@ -81,12 +93,12 @@ def getOptions():
 	parser.add_argument("bugNum", help="the bug number (ex: 1,2,3,4,...)")
 	parser.add_argument("buggyFolder", help="folder to check out buggy version of the bug")
 	parser.add_argument("fixedFolder", help="folder to check out fixed version of the bug")
-	#parser.add_argument("--genTool", help="3th param is the generation tool (Randoop or Evosuite)", default="Randoop")
-	parser.add_argument("--testSuiteFolder", help="the path where the test suite is located, starting from the the D4J_HOME folder (Example: generatedTestSuites)")
+	parser.add_argument("testSuiteFolder", help="the path where the test suite is located, starting from the the D4J_HOME folder (Example: generatedTestSuites)")
+	parser.add_argument("--genTool", help="3th param is the generation tool (Randoop or Evosuite)", default="Evosuite")
 	parser.add_argument("--coverage", help="a coverage file")
 	parser.add_argument("--file1", help="test parameter, first file to diff")
 	parser.add_argument("--file2", help="test parameter, second file to diff")
-       
+ 
 	return parser.parse_args()
 
 
@@ -96,13 +108,14 @@ def main():
 	# also, line wrap this file at 80 characters or so
 	# and make your argument description sentences shorter
 
-	bug = BugInfo(args.project, args.bugNum, args.buggyFolder, args.fixedFolder)
+	bug = BugInfo(args.project, args.bugNum, args.buggyFolder, args.fixedFolder, args.testSuiteFolder)
+	generateCovXML(bug,args.genTool)
+	for f in getEditedFiles(bug):
+		listOfChangedLines = getADiff(bug.getBugPath(),bug.getFixPath(), f, bug)
 
-        for f in getEditedFiles(bug):
-                print getADiff(bug.getBugPath(),bug.getFixPath(), f, bug)
 
-        if(not(args.file1 is None) and (not (args.file2 is None))):
-                getADiff(args.file1, args.file2)
+	if(not(args.file1 is None) and ( not (args.file2 is None))):
+		getADiff(args.file1, args.file2)
 	#print "the project you specified is: " + args.project
 
 main()
