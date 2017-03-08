9c9a8b61d78b3de37342bdca621091b8a6946a37
Claire Le Goues <clegoues@cs.cmu.edu>
2017-03-08 16:04:28 -0500

    first part of handling lists of bugs done

--- defects4j-scripts/getCoverage.py
+++ defects4j-scripts/getCoverage.py
@@ -9,12 +9,12 @@ d4jHome = os.environ['D4J_HOME']
 defects4jCommand = d4jHome + "/framework/bin/defects4j"
 
 class BugInfo(object):
-	def __init__(self, project, bugNum, wd, testSuitePath):
+	def __init__(self, project, bugNum, wd, testDir):
 		self.project = project
 		self.bugNum = bugNum
 		self.buggyFolder = wd + "/" + project.lower() + str(bugNum) + "Buggy"
-		self.fixedFolder = wd + "/" + project.lower() + str(bugNum) + "Buggy"
-		self.testSuitePath = testSuitePath
+		self.fixedFolder = wd + "/" + project.lower() + str(bugNum) + "Fixed"
+		self.testDir = testDir
 		self.ensureVersionAreCheckedOut()
 
 	def getProject(self):
@@ -29,8 +29,8 @@ class BugInfo(object):
 	def getBugPath(self):
 		return str(os.path.join(d4jHome, self.buggyFolder))
 
-	def getTestSuitePath(self):
-		return str(os.path.join(d4jHome, self.testSuitePath))
+	def getTestDir(self):
+		return str(os.path.join(d4jHome, self.testDir))
 	
 	def ensureVersionAreCheckedOut(self):
 		if(not os.path.exists(self.getBugPath())):
@@ -86,11 +86,12 @@ def printMethodCorrespondingToLine(lineNum, tree):
 	return methodsChanged
 
 def generateCovXML(bug, tool, seed):
-	if(tool == "Evosuite"):
+	if(tool.lower() == "evosuite"):
 		testSuiteName="evosuite-branch"
-	elif(tool == "Randoop"):
+	elif(tool.lower() == "randoop"):
 		testSuiteName="randoop"
-	cmd = defects4jCommand + " coverage -w " + bug.getFixPath() + " -s " +  bug.getTestSuitePath()+bug.getProject()+"-"+bug.getBugNum()+"f-"+testSuiteName+"."+str(seed)+".tar.bz2"
+	suitePath =  os.path.join(bug.getTestDir(), bug.getProject()+"-"+bug.getBugNum()+"f-"+testSuiteName+"."+str(seed)+".tar.bz2")
+	cmd = defects4jCommand + " coverage -w " + bug.getFixPath() + " -s " + str(suitePath)
 	subprocess.call(cmd, shell=True) # this doesn't save the log or do any kind of error checking (yet!)
 
 def getEditedFiles(bug):
@@ -107,36 +108,52 @@ def getADiff(buggyPath, fixedPath, pathToFile, bug):
 	#print pathToSource
 
         cmd = "diff --unchanged-line-format=\"\"  --old-line-format=\"%dn \" --new-line-format=\"%dn \" " + buggyPath+"/"+pathToSource+"/"+pathToFile +" " + fixedPath+"/"+pathToSource+"/"+pathToFile
-        #print cmd
+#        print cmd
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         for line in p.stdout:
-                diffLines = line
-	#print "Diff lines: "+diffLines
+#			print "Line: " + line
+			diffLines = line
+#	print "Diff lines: "+diffLines
 	return diffLines.split()
 	
 
 def getOptions():
 	parser = argparse.ArgumentParser(description="This script checks if a test suite is covering the human changes")
-	parser.add_argument("project", help="the project in upper case (ex: Lang, Chart, Closure, Math, Time)")
-	parser.add_argument("bugNum", help="the bug number (ex: 1,2,3,4,...)")
 	parser.add_argument("wd", help="working directory to check out project versions")
-	parser.add_argument("testSuite", help="the path where the test suite is located, starting from the the D4J_HOME folder (Example: generatedTestSuites)")
+	parser.add_argument("testDir", help="the path where the test suite is located, starting from the the D4J_HOME folder (Example: generatedTestSuites)")
+	parser.add_argument("--project", help="the project in upper case (ex: Lang, Chart, Closure, Math, Time)")
+	parser.add_argument("--bug", help="the bug number (ex: 1,2,3,4,...)")
+	parser.add_argument("--many", help="file listing bugs to process: project,bugNum (one per line). Lines starting with # are skipped")
 	parser.add_argument("--tool", help="the generation tool (Randoop or Evosuite)", default="Evosuite")
 	parser.add_argument("--seed", help="the seed the test suite was created with", default="1")
 	parser.add_argument("--coverage", help="a coverage file")
 	return parser.parse_args()
 
-
+#args.many is assumed to be not None
+def getAllBugs(bugs,args):
+	if(not os.path.isfile(args.many)):
+		# MAU TODO: ADD GRACEFUL ERROR HANDLING/MESSAGE HERE
+		return
+	else:
+		with open(args.many) as f:
+			pairs = [x.strip().split(',') for x in f.readlines() if x[0] != '#']
+			for pair in pairs:
+				bug = BugInfo(pair[0], int(pair[1]), args.wd, args.testDir)
+				bugs.append(bug)
 def main():
 	args=getOptions()
 	# TODO: insert error handling/sanity checking to be sure the appropriate environment variables are set and abort with an error/usage message if not
 	# TODO: line wrap this file at 80 characters or so
-
-	bug = BugInfo(args.project, args.bugNum, args.wd, args.testSuite)
-	if((args.coverage == None) and (not os.path.exists(bug.getFixPath()+"/coverage.xml"))):
-		generateCovXML(bug,args.tool, args.seed)
-	for f in getEditedFiles(bug):
-		listOfChangedLines = getADiff(bug.getBugPath(),bug.getFixPath(), f, bug)
-		computeCoverage(listOfChangedLines, bug.getFixPath()+"/coverage.xml")
+	bugs = []
+	if(args.many == None):
+		bugs.append(BugInfo(args.project, args.bug, args.wd, args.testDir))
+	else:
+		getAllBugs(bugs, args)
+	for bug in bugs:
+		if((args.coverage == None) and (not os.path.exists(bug.getFixPath()+"/coverage.xml"))):
+			generateCovXML(bug,args.tool, args.seed)
+		for f in getEditedFiles(bug):
+			listOfChangedLines = getADiff(bug.getBugPath(),bug.getFixPath(), f, bug)
+			computeCoverage(listOfChangedLines, bug.getFixPath()+"/coverage.xml")
 
 main()
