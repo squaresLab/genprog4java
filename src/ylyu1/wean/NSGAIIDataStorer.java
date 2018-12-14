0b27aa8f524b3252071680489bcec124c38cb624
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2018-12-14 12:39:39 -0500

    fixed bug w/ nsgaii serialization

--- src/ylyu1/wean/NSGAIIDataStorer.java
+++ src/ylyu1/wean/NSGAIIDataStorer.java
@@ -11,9 +11,9 @@ public class NSGAIIDataStorer implements Serializable {
 	public boolean good;
 	public int variant;
 	public String errorMessage;
-	public static ArrayList<ArrayList<Integer>> divscores; //collected even if diversity is not an objective
-	public static ArrayList<ArrayList<Pair<Integer, Double>>> nsgaiiFitnesses = new ArrayList<>(); //left elem of Pair is domination rank, right elem of Pair is crowding distance
-	public static ArrayList<ArrayList<Map<Class<?>, Double>>> objectiveValues = new ArrayList<>(); //maps Objective class -> Objective value
+	public ArrayList<ArrayList<Integer>> divscores; //collected even if diversity is not an objective
+	public ArrayList<ArrayList<Pair<Integer, Double>>> nsgaiiFitnesses = new ArrayList<>(); //left elem of Pair is domination rank, right elem of Pair is crowding distance
+	public ArrayList<ArrayList<Map<Class<?>, Double>>> objectiveValues = new ArrayList<>(); //maps Objective class -> Objective value
 	
 	public NSGAIIDataStorer(boolean good1, boolean repair1, int variant1, String errorMessage1, ArrayList<ArrayList<Integer>> divscores1,
 			ArrayList<ArrayList<Pair<Integer, Double>>> nsgaiiFits, ArrayList<ArrayList<Map<Class<?>, Double>>> objectiveVals)
