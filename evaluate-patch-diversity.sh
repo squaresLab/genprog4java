05ca47a3820caef753e7e5fd9d2a0b4e3ec00242
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-05 15:47:42 -0400

    script mistake

--- evaluate-patch-diversity.sh
+++ evaluate-patch-diversity.sh
@@ -1,7 +1,7 @@
 #this is a convenience script for using in Zhen's docker containers
 
-$1=$BUGWD
-$GP4J_PDE=/home/user/gp4j-patch-div-eval
+1=$BUGWD
+GP4J_PDE=/home/user/gp4j-patch-div-eval
 
 java -ea =Dlog4j.configurationFile=file:"$GP4J_PDE"/src/log4j.properties -Dfile.encoding=UTF-8 \
 	-classpath "$GP4J_PDE"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar \
