32abbab883f8df13ebdcfec128a124d3658aff6c
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-05 15:46:18 -0400

    print instead of redirect for now

--- evaluate-patch-diversity.sh
+++ evaluate-patch-diversity.sh
@@ -6,4 +6,4 @@ $GP4J_PDE=/home/user/gp4j-patch-div-eval
 java -ea =Dlog4j.configurationFile=file:"$GP4J_PDE"/src/log4j.properties -Dfile.encoding=UTF-8 \
 	-classpath "$GP4J_PDE"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar \
 	clegoues.genprog4java.main.Main \
-	$GP4J_PDE $JAVA_HOME $DAIKONDIR $BUGWD/introclass.config | tee $BUGWD/log-eval-patch-diversity.txt
+	$GP4J_PDE $JAVA_HOME $DAIKONDIR $BUGWD/introclass.config
