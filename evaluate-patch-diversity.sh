8d4f7fa01783721ff06d51974c264840d8f25338
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-05 15:38:31 -0400

    edit convenience script

--- evaluate-patch-diversity.sh
+++ evaluate-patch-diversity.sh
@@ -1,8 +1,9 @@
 #this is a convenience script for using in Zhen's docker containers
 
-$1 = $BUGWD
+$1=$BUGWD
+$GP4J_PDE=/home/user/gp4j-patch-div-eval
 
-java -ea =Dlog4j.configurationFile=file:"$GP4J_HOME"/src/log4j.properties -Dfile.encoding=UTF-8 \
-	-classpath "$GP4J_HOME"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar \
+java -ea =Dlog4j.configurationFile=file:"$GP4J_PDE"/src/log4j.properties -Dfile.encoding=UTF-8 \
+	-classpath "$GP4J_PDE"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar \
 	clegoues.genprog4java.main.Main \
-	$GP4J_HOME $JAVA_HOME $DAIKONDIR $BUGWD/introclass.config | tee $BUGWD/log-eval-patch-diversity.txt
+	$GP4J_PDE $JAVA_HOME $DAIKONDIR $BUGWD/introclass.config | tee $BUGWD/log-eval-patch-diversity.txt
