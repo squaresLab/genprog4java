1499bdd5288544085baa16e607f44b4eb865622e
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-05 15:48:26 -0400

    and yet another mistake

--- evaluate-patch-diversity.sh
+++ evaluate-patch-diversity.sh
@@ -3,7 +3,7 @@
 1=$BUGWD
 GP4J_PDE=/home/user/gp4j-patch-div-eval
 
-java -ea =Dlog4j.configurationFile=file:"$GP4J_PDE"/src/log4j.properties -Dfile.encoding=UTF-8 \
+java -ea -Dlog4j.configurationFile=file:"$GP4J_PDE"/src/log4j.properties -Dfile.encoding=UTF-8 \
 	-classpath "$GP4J_PDE"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar \
 	clegoues.genprog4java.main.Main \
 	$GP4J_PDE $JAVA_HOME $DAIKONDIR $BUGWD/introclass.config
