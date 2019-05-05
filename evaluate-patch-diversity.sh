6de40679e290cd8183afa3740f9bcfe6120ff1e1
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-05 15:50:41 -0400

    shell scripts stink

--- evaluate-patch-diversity.sh
+++ evaluate-patch-diversity.sh
@@ -1,9 +1,12 @@
 #this is a convenience script for using in Zhen's docker containers
 
-BUGWD = $1
+BUGWD=$1
 GP4J_PDE=/home/user/gp4j-patch-div-eval
 
+BASEDIR=$PWD
+cd $BUGWD
 java -ea -Dlog4j.configurationFile=file:"$GP4J_PDE"/src/log4j.properties -Dfile.encoding=UTF-8 \
 	-classpath "$GP4J_PDE"/target/uber-GenProg4Java-0.0.1-SNAPSHOT.jar \
 	clegoues.genprog4java.main.Main \
 	$GP4J_PDE $JAVA_HOME $DAIKONDIR $BUGWD/introclass.config
+cd $BASEDIR
